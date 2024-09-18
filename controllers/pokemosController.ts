4e273e6b11e93132d90d0b6325e6fdcde71a0172
dyeyo <dyegovallejo@gmail.com>
2024-09-18 11:03:38 -0500

    Documentacion

--- controllers/pokemosController.ts
+++ controllers/pokemosController.ts
@@ -5,6 +5,45 @@ import {
   getPokemonDetails,
 } from "../services/pokemonServices";
 
+/**
+ * @swagger
+ * /api/pokemons:
+ *   get:
+ *     summary: Obtiene la lista de Pokémon
+ *     description: Este endpoint retorna una lista de todos los Pokémon disponibles en la base de datos.
+ *     responses:
+ *       200:
+ *         description: Lista de Pokémon
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "Bulbasaur"
+ *                   type:
+ *                     type: array
+ *                     items:
+ *                       type: string
+ *                       example: "Grass"
+ *       500:
+ *         description: Error en el servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
+
 export const getListPokemons = async (req: Request, res: Response) => {
   try {
     const pokemons = await getPokemons();
@@ -14,6 +53,59 @@ export const getListPokemons = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * @swagger
+ * /api/pokemons/{id}:
+ *   get:
+ *     summary: Obtiene un Pokémon por ID
+ *     description: Este endpoint retorna la información de un Pokémon específico basado en su ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: El ID del Pokémon que deseas obtener.
+ *     responses:
+ *       200:
+ *         description: Información del Pokémon
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   example: "Bulbasaur"
+ *                 type:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     example: "Grass"
+ *       404:
+ *         description: Pokémon no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Pokémon no encontrado"
+ *       500:
+ *         description: Error en el servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
 export const getOnePokemon = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
@@ -25,6 +117,59 @@ export const getOnePokemon = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * @swagger
+ * /api/pokemons/{id}/type:
+ *   get:
+ *     summary: Obtiene un Pokémon con su tipo por ID
+ *     description: Este endpoint retorna la información de un Pokémon específico basado en su ID, incluyendo detalles sobre su tipo.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: El ID del Pokémon que deseas obtener.
+ *     responses:
+ *       200:
+ *         description: Información del Pokémon con su tipo
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   example: "Bulbasaur"
+ *                 type:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                     example: "Grass"
+ *       404:
+ *         description: Pokémon no encontrado
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Pokémon no encontrado"
+ *       500:
+ *         description: Error en el servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: "Internal Server Error"
+ */
 export const getPokemonWithType = async (req: Request, res: Response) => {
   const { id } = req.params;
   console.log(id);
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
+import { options } from "./swaggerOptions";
 
 import routePokemons from "./routers/pokemons.routes";
 
@@ -9,10 +12,12 @@ const app: Express = express();
 
 
 app.use(express.json());
+const specs = swaggerJsDoc(options);
 
 // Rutas
-app.use("/api/pokemons", routePokemons);
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
+app.use("/api/pokemons", routePokemons);
 const PORT: number = parseInt(process.env.PORT || "3000", 10);
 
 app.listen(PORT, () => {
